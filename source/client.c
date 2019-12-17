03f91138966096ecbd1f6977de54e1125105492d
ghost171 <55321108+ghost171@users.noreply.github.com>
2019-12-17 15:26:29 +0600

    Add files via upload

--- source/client.c
+++ source/client.c
@@ -1,95 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <unistd.h>
-#define MAX 80
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-enum errors {
-    OK,
-    ERR_INCORRECT_ARGS,
-    ERR_SOCKET,
-    ERR_CONNECT
-};
+#define PORT 8080
 
-int init_socket(const char *ip, int port) {
-    //open socket, result is socket descriptor
-    int server_socket = socket(PF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0) {
-        perror("Fail: open socket");
-        _exit(ERR_SOCKET);
-    }
+int main(){
 
-    //prepare server address
-    struct hostent *host = gethostbyname(ip);
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(port);
-    memcpy(&server_address.sin_addr, host -> h_addr_list[0], sizeof(server_address));
+	int clientSocket, ret;
+	struct sockaddr_in serverAddr;
+	char buffer[1024];
 
-    //connection
-    struct sockaddr_in sin;
-    sin.sin_family = AF_INET;
-    sin.sin_port = htons(port);
-    memcpy(&sin.sin_addr, host->h_addr_list[0], sizeof(sin.sin_addr));
-    if (connect(server_socket, (struct sockaddr*) &sin, sizeof(sin)) < 0) {
-        perror("Fail: connect");
-        _exit(ERR_CONNECT);
-    }
-    return server_socket;
-}
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if(clientSocket < 0){
+		printf("[-]Error in connection.\n");
+		exit(1);
+	}
+	printf("[+]Client Socket is created.\n");
 
-void func(int sockfd) 
-{ 
-    char buff[MAX]; 
-    int n; 
-    for (;;) { 
-        bzero(buff, sizeof(buff)); 
-        printf("Enter the string : "); 
-        n = 0; 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
-        write(sockfd, buff, sizeof(buff)); 
-        bzero(buff, sizeof(buff)); 
-        read(sockfd, buff, sizeof(buff)); 
-        printf("From Server : %s", buff); 
-        if ((strncmp(buff, "exit", 4)) == 0) { 
-            printf("Client Exit...\n"); 
-            break; 
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	if(ret < 0){
+		printf("[-]Error in connection.\n");
+		exit(1);
+	}
+	printf("[+]Connected to Server.\n");
+
+	while(1){
+		printf("Client: \t\n");
+        if(fork() == 0){
+            int k;
+            k = read(0, buffer, 1000);
+            send(clientSocket, buffer, k, 0);
         }
-    } 
-} 
+		if(strcmp(buffer, ":exit") == 0){
+			close(clientSocket);
+			printf("[-]Disconnected from server.\n");
+			exit(1);
+		}
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        puts("Incorrect args.");
-        puts("./server <ip> <port>");
-        puts("Example:");
-        puts("./server 127.0.0.1 5005");
-        return ERR_INCORRECT_ARGS;
-    }
-    char message[LINE_MAX];
-    char *ip = argv[1];
-    int port = atoi(argv[2]);
-    int server = init_socket(ip, port);
-    while(1) {
-        if (recv(server, message, 256, 0) < 0) {
-            err(1, NULL);
-        }
-        puts(message);
-        printf("Enter a message: ");
-        fgets(message, 256, stdin);
-        if (strcmp(message, "exit") == 0) {
-            puts("Client exited");
-            break;
-        }
-        send(server, message, strlen(message), 0);
-    }
-    close(server);
-    return OK;
+		if(recv(clientSocket, buffer, 1024, 0) < 0){
+			printf("[-]Error in receiving data.\n");
+		}else{
+			printf("Server: \t%s\n", buffer);
+		}
+	}
+
+	return 0;
 }
--- source/server.c
+++ source/server.c
@@ -1,162 +1,79 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <err.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#define MAX 80
+#include <stdio.h>
 
-enum errors {
-    OK,
-    ERR_INCORRECT_ARGS,
-    ERR_SOCKET,
-    ERR_SETSOCKETOPT,
-    ERR_BIND,
-    ERR_LISTEN
-};
-
-int init_socket(int port) {
-    //open socket, return socket descriptor
-    int server_socket = socket(PF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0) {
-        perror("Fail: open socket");
-        _exit(ERR_SOCKET);
+void reciever ( int id , int input , int output) {
+    size_t size;
+    char buf[100];
+    while ( (size = read ( input , &buf, 100)) > 0){
+        printf("got %zu bytes from client socket %d, content %s\n", size, id, buf);
+        /* critical start */
+        write ( output , & id , sizeof ( id ));
+        write ( output , & size , sizeof ( size ));
+        write ( output , buf , size );
+        /* critical stop */
     }
- 
-    //set socket option
+}
+
+int main(int argc, char ** argv){
+    int server_socket = socket(PF_INET, SOCK_STREAM, 0);
     int socket_option = 1;
     setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &socket_option, sizeof(socket_option));
-    if (server_socket < 0) {
-        perror("Fail: set socket options");
-        _exit(ERR_SETSOCKETOPT);
-    }
+    // set socket address
+    struct sockaddr_in server_address ;
+    server_address . sin_family = AF_INET ;
+    server_address . sin_port = htons (8080);
+    server_address . sin_addr . s_addr = INADDR_ANY ;
+    bind ( server_socket , ( struct sockaddr *) & server_address , sizeof ( server_address ));
+    // start listen mode
+    listen ( server_socket , 5);
 
-    //set socket address
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(port);
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
-        perror("Fail: bind socket address");
-        _exit(ERR_BIND);
+    int clientsNum = 2;
+    struct sockaddr_in client[100] ;
+    struct  sockaddr  * client_ptr[100];
+    for(int i = 0; i < 100; i++){
+        client_ptr[i] = (struct sockaddr *)&client[i];
     }
-
-    //listen mode start
-    if (listen(server_socket, 5) < 0) {
-        perror("Fail: bind socket address");
-        _exit(ERR_LISTEN);
+    socklen_t size ;
+    int client_socket[100];
+    for(int i = 0; i < clientsNum; i++){
+        client_socket[i] = accept(server_socket, client_ptr[i], &size);
+        char * addr = inet_ntoa ( client[i].sin_addr );
+        int port = ntohs ( client[i].sin_port );
+        printf ( "connected: %s %d \n " , addr , port );
     }
-    return server_socket;
-}
-
-struct sockaddr_in *clients_init(int port, int count_of_users, int **sockets){
-    struct sockaddr_in *server_address = NULL;
-    int errno;
-    for (int i = 0; i < count_of_users; i++) {
-        server_address = (struct sockaddr_in *)realloc(server_address, (i + 1) * sizeof(struct sockaddr_in));
-        server_address[i].sin_family = AF_INET;
-        server_address[i].sin_addr.s_addr = INADDR_ANY;
-        server_address[i].sin_port = htons(port + i);
-        errno = bind(*sockets[i], (struct sockaddr *) &server_address[i], sizeof(server_address[i]));
-        if (errno) {
-            err(1, NULL);
-        }
-    }
-    return server_address;
-}
-
-int *client_sockets_init(int count_of_users)
-{
-    int *server_sockets = NULL;
-    for(int i = 0; i < count_of_users; i++) {
-        server_sockets = (int *)realloc(server_sockets, (i + 1) * sizeof(int));
-        server_sockets[i] = socket(PF_INET, SOCK_STREAM, 0);
-    }
-    return server_sockets;
-}
-
-void receiver(unsigned int fd_in, unsigned int fd_out){
-    unsigned char id, size;
-    char buf[256];
-    int ret;
-    while(1){
-        ret = read(fd_in,  &size, 1);
-        if(ret < 0){
-            exit(1);
-        }
-        read(fd_in, buf, size);
-        write(fd_out, &id, 1);
-        write(fd_out, &size, 1);
-        write(fd_out, buf, size);
-    }
-}
-
-char *read_fd(ssize_t fd, char *out, int size)
-{
-    if (read(fd, out, size) < 0) {
-        err(1, NULL);
-    }
-    return out;
-}
-
-void write_fd(char *str, ssize_t fd)
-{
-    if (write(fd, str, sizeof(str)) < 0) {
-        err(1, NULL);
-    }
-}
-
-int main(int argc, char** argv) {
-    if (argc != 3) {
-        puts("Incorrect args.");
-        puts("./server <port>");
-        puts("Example:");
-        puts("./server 8000");
-        return ERR_INCORRECT_ARGS;
-    }
-    int i = 0, port = atoi(argv[1]), users = atoi(argv[2]);
-    int server_socket = init_socket(port);
-    int *client_socket = client_sockets_init(users);
-    struct sockaddr_in *clients = clients_init(port, users, &client_socket);
-    for (i = 0; i < users; i++) {
-        puts("Wait for connection");
-        struct sockaddr_in client_address;
-        socklen_t size;
-        client_socket[i] = accept(server_socket, 
-                                    (struct sockaddr *) &client_address,
-                                    &size);
-        printf("connected: %s %d\n", inet_ntoa(client_address.sin_addr),
-                                        ntohs(client_address.sin_port));
-    }
-    int pd[2];
-    int pid[105];
-    pipe(pd);
-    for (i = 0; i < users; i++) {
-        pid[i] = fork();
-        if (pid[i] == 0) {
-            close(pd[0]);
-            receiver(client_socket[i], pd[1]);
-            close(pd[1]);
+    int receiver_2_sender[2];
+    pipe ( receiver_2_sender );
+    for ( int i = 0; i < clientsNum ; i++){
+        if ( fork() == 0) {
+            printf("client %d process started\n", i);
+            reciever (i,  client_socket[i], receiver_2_sender[1]);
+            close ( client_socket [i]);
             return 0;
         }
     }
-    close(pd[1]);
+    
+    char buf[100];
+    size_t size1;
+    int id = 0;
+
+    printf("starting main fan out\n");
     while (1) {
-        char id;
-        char size;
-        char out[256];
-        read_fd(pd[0], &id, 1);
-        read_fd(pd[0], &size, 1);
-        read_fd(pd[0], out, size);
-        for (i = 0; i < users; i++) {
-            write_fd(&id, client_socket[i]);
-            write_fd(&size, client_socket[i]);
-            write_fd(out, client_socket[i]);
+        read ( receiver_2_sender [0] , &id , sizeof ( id ));
+        read ( receiver_2_sender [0] , &size1 , sizeof ( size1 ));
+        read ( receiver_2_sender [0] , buf , size1 );
+        printf("got from pipe client id %d, content %s\n", id, buf);
+        for ( int i = 0; i < clientsNum ; i ++) {
+            printf("sending '%s' to client %d\n", buf, i);
+            write(client_socket[i], buf, size1);
         }
-        
     }
-    return OK;
+    printf("stopping main fan out\n");
+    close ( receiver_2_sender [1]);
+    close ( receiver_2_sender [0]);
+    return 0;
 }
