2e042284d364363e4842f73fa900ab88b81e7dea
ghost171 <55321108+ghost171@users.noreply.github.com>
2019-12-17 18:26:34 +0600

    Bad chat

--- source/client.c
+++ source/client.c
@@ -13,7 +13,6 @@ int main(){
 
 	int clientSocket, ret;
 	struct sockaddr_in serverAddr;
-	char buffer[1024];
 
 	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if(clientSocket < 0){
@@ -26,31 +25,43 @@ int main(){
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(PORT);
 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
+    //char nick[100];
+    //scanf("%s", nick);
 	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
 	if(ret < 0){
 		printf("[-]Error in connection.\n");
 		exit(1);
 	}
 	printf("[+]Connected to Server.\n");
+    //send(clientSocket, nick, strlen(nick));
+    if(fork() == 0){
+        while (1) {
+        	char buffer[1024];
+            scanf("%s", buffer);
+            send(clientSocket, buffer, strlen(buffer), 0);
 
-	while(1){
-		printf("Client: \t\n");
-        if(fork() == 0){
-            int k;
-            k = read(0, buffer, 1000);
-            send(clientSocket, buffer, k, 0);
+            printf("Client: \t\n");
+            if(strcmp(buffer, ":exit") == 0){
+                close(clientSocket);
+                printf("[-]Disconnected from server.\n");
+                exit(1);
+            }
         }
-		if(strcmp(buffer, ":exit") == 0){
-			close(clientSocket);
-			printf("[-]Disconnected from server.\n");
-			exit(1);
-		}
-
-		if(recv(clientSocket, buffer, 1024, 0) < 0){
+        return 0;
+    }
+	while(1){
+    	char buffer[1024];
+        ssize_t size;
+        size = recv(clientSocket, buffer, 1000, 0);
+		if(size < 0){
 			printf("[-]Error in receiving data.\n");
-		}else{
-			printf("Server: \t%s\n", buffer);
+            break;
+		}else if (size == 0) {
+            printf("Server closed connection\n");
+            break;
+        }else{
+            buffer[size] = '\0';
+			printf("Server: %s\n", buffer);
 		}
 	}
 
